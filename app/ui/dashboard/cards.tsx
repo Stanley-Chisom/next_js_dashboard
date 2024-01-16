3cb4d67c4b9931f4accdeda2cfd3eafaed42c52b
Stanley Chisom <stanley.chisom042@gmail.com>
2024-01-16 22:38:01 +0100

    search and pagination

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,7 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,6 +15,13 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
@@ -27,7 +35,7 @@ export default async function CardWrapper() {
         type="customers"
       />
     </>
- );
+  );
 }
 
 export function Card({
